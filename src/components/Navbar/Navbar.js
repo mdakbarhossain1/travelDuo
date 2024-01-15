fae8442f596ada59b78f53d0eef582e8c35a2187
As-Shovo <akbarshovo18@gmail.com>
2024-01-16 02:27:56 +0600

    navbar responsive and css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 import Link from 'next/link';
 
 const Navbar = () => {
+
+    const [navbar, setNavbar] = useState(true);
+
+    const showSidebar = () => setNavbar(!navbar);
+
     return (
         <div>
             <header className={styles.header}>
                 <div className={`${styles.section__container} ${styles.header__container}`}>
                     <nav>
-                        <div className={styles.nav__menu__btn} id="menu-btn">
+                        <div className={styles.nav__menu__btn} onClick={showSidebar}>
                             <span><i className={styles.ri_menu_2_line}></i></span>
                             <h4>Menu</h4>
                         </div>
-                        <ul className={styles.nav__links} id="nav-links">
-                            <Link href="/">Home</Link>
-                            <Link href="/travel">Travel</Link>
-                            <Link href="/discover">Discover</Link>
-                            <Link href="/clients">Client</Link>
-                            <Link href="/about">About</Link>
+                        <ul className={`${styles.nav__links} ${navbar?styles.open:styles.close} `} id="nav-links">
+                            <Link onClick={showSidebar} href="/">Home</Link>
+                            <Link onClick={showSidebar} href="/travel">Travel</Link>
+                            <Link onClick={showSidebar} href="/discover">Discover</Link>
+                            <Link onClick={showSidebar} href="/clients">Client</Link>
+                            <Link onClick={showSidebar} href="/about">About</Link>
                         </ul>
                     </nav>
                    
